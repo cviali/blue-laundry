fdd615a2ec771634b24e4fa6c57f3602bfb0223f
Christian Viali <37096704+cviali@users.noreply.github.com>
2018-12-17 22:58:26 +0700

    More UI changes to complement UI Design
    Changes:
    - Creating java class for each planned fragments
    - NavigationView: added a design and text display changed.
    - Adding placeholder fragment controller in Home.java

--- app/src/main/java/binus/viali/bluelaundry/Home.java
+++ app/src/main/java/binus/viali/bluelaundry/Home.java
@@ -1,13 +1,19 @@
 package binus.viali.bluelaundry;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private TextView greetings;
     private DrawerLayout mDrawerLayout;
@@ -20,18 +26,19 @@ public class Home extends AppCompatActivity {
 
         greetings = findViewById(R.id.text_greeting);
 
-        String username = getIntent().getStringExtra(Login.EXTRA_USERNAME);
+    String username = getIntent().getStringExtra(Login.EXTRA_USERNAME);
 
         greetings.setText("Welcome, " + username);
 
-        mDrawerLayout = findViewById(R.id.drawer_layout);
-        mToggle = new ActionBarDrawerToggle(Home.this, mDrawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+    mDrawerLayout = findViewById(R.id.drawer_layout);
+    mToggle = new ActionBarDrawerToggle(Home.this, mDrawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
+    getSupportActionBar().setTitle("Blue Laundry");
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+}
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -39,5 +46,32 @@ public class Home extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        displaySelectedScreen(menuItem.getItemId());
+        return true;
+    }
+
+    private void displaySelectedScreen(int itemId){
+        //Fragment fragment = null;
+        switch (itemId){
+            case R.id.nav_home:
+                Toast.makeText(getApplicationContext(),"Ini Home Mas", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_items:
+                Toast.makeText(getApplicationContext(),"Ini Items Mas", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_about:
+                Toast.makeText(getApplicationContext(),"Ini About Mas", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_logout:
+                Toast.makeText(getApplicationContext(),"Ini Logout Mas", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
     }
 }
